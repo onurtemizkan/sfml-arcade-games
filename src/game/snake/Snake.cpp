e9766bc05dad59fe1fe04a2cd593b3b578c8509d
Onur Temizkan <onurtemizkan@gmail.com>
2016-03-07 21:36:24 +0200

    converted games to classes

---
+++ src/game/snake/Snake.cpp
@@ -0,0 +1,5 @@
+//
+// Created by onur on 07/03/16.
+//
+
+#include "Snake.h"
---
+++ src/game/snake/Snake.h
@@ -0,0 +1,14 @@
+//
+// Created by onur on 07/03/16.
+//
+
+#ifndef SFML_GAMES_SNAKE_H
+#define SFML_GAMES_SNAKE_H
+
+
+class Snake {
+
+};
+
+
+#endif //SFML_GAMES_SNAKE_H
