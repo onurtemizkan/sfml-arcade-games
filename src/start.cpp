fcb0a6af5ea9feb75e54af133a28b3a3d78d4dae
Onur Temizkan <onurtemizkan@gmail.com>
2016-03-07 21:55:28 +0200

    main.cpp -> src/start.cpp

--- src/start.cpp
+++ src/start.cpp
@@ -2,8 +2,8 @@
 // Created by onur on 07/03/16.
 //
 #include <iostream>
-#include "src/game/pong/Pong.cpp"
-#include "src/game/arkanoid/Arkanoid.cpp"
+#include "game/pong/Pong.cpp"
+#include "game/arkanoid/Arkanoid.cpp"
 
 using namespace std;
 
