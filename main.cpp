7b4a84fc6fbe14ec1fecd155f7d609a21f7c085d
Onur Temizkan <onurtemizkan@gmail.com>
2016-03-07 21:46:41 +0200

    added .gitignore and main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by onur on 07/03/16.
+//
+#include <iostream>
+#include "src/game/pong/Pong.cpp"
+#include "src/game/arkanoid/Arkanoid.cpp"
+
+using namespace std;
+
+int main() {
+//    Pong pong;
+//    pong.start();
+    Arkanoid arkanoid;
+    arkanoid.start();
+}
